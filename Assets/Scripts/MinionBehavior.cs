6d67d72abf5b550a6c3bd576b2f419bb696495c7
DanielSelnick <daniel@danielselnick.com>
2012-09-30 18:10:17 -0700

    Added minion behavior thingy.

---
+++ Assets/Scripts/MinionBehavior.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class MinionBehavior : MonoBehaviour {
+	private Rigidbody _theRigidBody;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
